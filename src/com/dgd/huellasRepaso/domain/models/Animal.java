e43e5a62b5ba9e530ebe69c15860f9beb7e96dd3
diegogd04 <diego2612004@gmail.com>
2023-06-15 12:29:23 +0200

    clase animal

---
+++ src/com/dgd/huellasRepaso/domain/models/Animal.java
@@ -0,0 +1,40 @@
+package com.dgd.huellasRepaso.domain.models;
+
+public class Animal {
+    protected Integer id;
+    protected String nombre;
+    protected String fechaNacimiento;
+    protected String sexo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
