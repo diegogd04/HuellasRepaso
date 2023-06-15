ebbdc3124eb6b9966245d18c346cce81a0d64382
diegogd04 <diego2612004@gmail.com>
2023-06-15 12:35:31 +0200

    clase adoptante

---
+++ src/com/dgd/huellasRepaso/domain/models/Adoptante.java
@@ -0,0 +1,22 @@
+package com.dgd.huellasRepaso.domain.models;
+
+public class Adoptante extends Persona{
+    private String fechaNacimiento;
+    private String direccion;
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
