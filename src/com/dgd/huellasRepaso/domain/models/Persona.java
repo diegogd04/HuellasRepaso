076fa8d413c537dcee22d7f23d79a2aa9409532b
diegogd04 <diego2612004@gmail.com>
2023-06-15 12:34:08 +0200

    clase persona

---
+++ src/com/dgd/huellasRepaso/domain/models/Persona.java
@@ -0,0 +1,40 @@
+package com.dgd.huellasRepaso.domain.models;
+
+public class Persona {
+    protected Integer id;
+    protected String nombre;
+    protected String apellidos;
+    protected String dni;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
