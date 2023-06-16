1078f1dd276a3a07ee30550edde22f3b79c00ccc
diegogd04 <diego2612004@gmail.com>
2023-06-16 13:29:02 +0200

    Main y TerminalView

--- src/com/dgd/huellasRepaso/presentation/Main.java
+++ src/com/dgd/huellasRepaso/presentation/Main.java
@@ -2,6 +2,7 @@ package com.dgd.huellasRepaso.presentation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TerminalView terminalView = new TerminalView();
+        terminalView.init();
     }
 }
